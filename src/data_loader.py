47a3330710f93e7cceabfa1e636307309307914a
Bhoomi_Panchal_analytics <bhoomidpanchal@gmail.com>
2026-02-24 21:01:01 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,22 +1,49 @@
+# src/data_loader.py
+
 import yfinance as yf
 import pandas as pd
+from datetime import datetime, timedelta
 
-def fetch_stock_data(ticker: str, start="2018-01-01"):
 
-    try:
-        data = yf.download(
-            ticker,
-            start=start,
-            progress=False,
-            auto_adjust=True,
-            threads=False
-        )
+def get_last_5y_start():
+    return (datetime.today() - timedelta(days=5*365)).strftime("%Y-%m-%d")
 
-        if data is None or data.empty:
-            raise ValueError("No data fetched. Check ticker symbol.")
 
-        data.reset_index(inplace=True)
-        return data
+def fetch_multiple_tickers(tickers: list):
 
-    except Exception as e:
-        raise ValueError(f"Data fetch failed: {str(e)}")
+    start_date = get_last_5y_start()
+
+    data = yf.download(
+        tickers,
+        start=start_date,
+        auto_adjust=True,
+        progress=False,
+        threads=False,
+        group_by='ticker'
+    )
+
+    if data.empty:
+        raise ValueError("No data fetched from Yahoo.")
+
+    cleaned_data = {}
+
+    for ticker in tickers:
+        try:
+            df = data[ticker].copy()
+            df.reset_index(inplace=True)
+
+            required_cols = ["Date", "Open", "High", "Low", "Close", "Volume"]
+
+            if not all(col in df.columns for col in required_cols):
+                continue
+
+            df = df.dropna()
+            cleaned_data[ticker] = df
+
+        except Exception:
+            continue
+
+    if len(cleaned_data) == 0:
+        raise ValueError("No valid ticker data found.")
+
+    return cleaned_data
