4d007e93d4a719cb4d99c68ce16ae848d408be2c
Bhoomi_Panchal_analytics <bhoomidpanchal@gmail.com>
2026-02-24 21:31:54 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,49 +1,27 @@
-# src/data_loader.py
-
 import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
 
 
-def get_last_5y_start():
-    return (datetime.today() - timedelta(days=5*365)).strftime("%Y-%m-%d")
+def fetch_multiple_tickers(tickers):
 
-
-def fetch_multiple_tickers(tickers: list):
-
-    start_date = get_last_5y_start()
+    start = (datetime.today() - timedelta(days=5*365)).strftime("%Y-%m-%d")
 
     data = yf.download(
         tickers,
-        start=start_date,
+        start=start,
         auto_adjust=True,
         progress=False,
         threads=False,
         group_by='ticker'
     )
 
-    if data.empty:
-        raise ValueError("No data fetched from Yahoo.")
-
-    cleaned_data = {}
+    cleaned = {}
 
     for ticker in tickers:
-        try:
+        if ticker in data.columns.levels[0]:
             df = data[ticker].copy()
             df.reset_index(inplace=True)
+            cleaned[ticker] = df
 
-            required_cols = ["Date", "Open", "High", "Low", "Close", "Volume"]
-
-            if not all(col in df.columns for col in required_cols):
-                continue
-
-            df = df.dropna()
-            cleaned_data[ticker] = df
-
-        except Exception:
-            continue
-
-    if len(cleaned_data) == 0:
-        raise ValueError("No valid ticker data found.")
-
-    return cleaned_data
+    return cleaned
