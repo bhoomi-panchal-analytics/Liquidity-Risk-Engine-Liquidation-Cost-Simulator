66b98c793e60cca195d00f4da676997d6bb657b4
Bhoomi_Panchal_analytics <bhoomidpanchal@gmail.com>
2026-02-24 20:57:40 +0530

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,32 +1,22 @@
-
-
 import yfinance as yf
 import pandas as pd
-import os
 
-def fetch_stock_data(ticker: str, start: str = "2018-01-01", end: str = None):
-    """
-    Fetch historical OHLCV data from Yahoo Finance.
-    """
+def fetch_stock_data(ticker: str, start="2018-01-01"):
 
-    data = yf.download(ticker, start=start, end=end, progress=False)
+    try:
+        data = yf.download(
+            ticker,
+            start=start,
+            progress=False,
+            auto_adjust=True,
+            threads=False
+        )
 
-    if data.empty:
-        raise ValueError("No data fetched. Check ticker symbol.")
+        if data is None or data.empty:
+            raise ValueError("No data fetched. Check ticker symbol.")
 
-    data.reset_index(inplace=True)
+        data.reset_index(inplace=True)
+        return data
 
-    # Ensure required columns exist
-    required_cols = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
-    for col in required_cols:
-        if col not in data.columns:
-            raise ValueError(f"Missing column: {col}")
-
-    return data
-
-
-def save_raw_data(data: pd.DataFrame, ticker: str):
-    os.makedirs("data/raw", exist_ok=True)
-    file_path = f"data/raw/{ticker}_raw.csv"
-    data.to_csv(file_path, index=False)
-    return file_path
+    except Exception as e:
+        raise ValueError(f"Data fetch failed: {str(e)}")
